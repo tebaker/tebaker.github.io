8389600122cd8787cbb6453519654dd2d78cf147
tebaker <taalonn@gmail.com>
2018-02-25 12:07:56 -0800

    adding all new files for hw2 folder

--- hw2/js/create-skybox.js
+++ hw2/js/create-skybox.js
@@ -2,7 +2,7 @@
     size = h, w, d
     textures = array of 6 sides of cube texture
 */
-function createSkybox( size, textures ) {
+function createSkybox( size, textures, skyboxVS, skyboxFS ) {
     var cube = new THREE.BoxGeometry(size, size, size);
     var tex = new THREE.CubeTextureLoader().load(textures);
 
@@ -12,8 +12,8 @@ function createSkybox( size, textures ) {
 
     var material = new THREE.ShaderMaterial({
         uniforms: shaderInput,
-        vertexShader: vs_skybox,
-        fragmentShader: fs_skybox,
+        vertexShader: skyboxVS,
+        fragmentShader: skyboxFS,
     });
 
     material.side = THREE.BackSide;
